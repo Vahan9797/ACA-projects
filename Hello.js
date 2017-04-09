da56ff532e6644b05b068c97731b2470c6437553
Vahan Arakelyan <Vahan Arakelyan>
2017-04-09 12:14:08 +0400

    Resurrected.

--- Hello.js
+++ Hello.js
@@ -1,11 +1,11 @@
 var sum = 18;
 
 for (var i=1; i<=10; i++) {
-	sum = summary(sum, i);
+	sum = add(sum, i);
 }
 
 document.write(sum);
 
-function summary(x, y) {
+function add(x, y) {
 	return x + y;
 }
